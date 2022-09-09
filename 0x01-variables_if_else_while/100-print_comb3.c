806b19a90d06fd3c64baf388408b3b7964e0afd1
Iam_fortunatus <amfortunatus@gmail.com>
2022-09-09 22:39:48 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers with commas
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int i, e;
+	int i, j;
 
 	i = 48;
-	e = 48;
+	j = 48;
 
-	while (e < 58)
+	while (i < 58)
 	{
-		i = 48;
-		while (i < 58)
+		j = 48;
+		while (j < 58)
 		{
-			putchar(e);
 			putchar(i);
-			if (i == 57 && e == 57)
+			putchar(j);
+
+			if (i < 57 || j < 57)
 			{
-				break;
+				putchar(44);
+				putchar(32);
 			}
-			putchar(',');
-			putchar(' ');
-			i++;
+			j++;
 		}
-		e++;
+
+		i++;
 	}
-	putchar('\n');
+
+	putchar(10);
+
 	return (0);
 }
