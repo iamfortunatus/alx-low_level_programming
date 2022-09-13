4c91e4d265184950614e9766ad4531081834e77f
Iam_fortunatus <amfortunatus@gmail.com>
2022-09-13 13:14:09 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	char *sh = "_putchar";
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	while (*sh)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(*sh);
-		sh++;
+		_putchar(str[count]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
