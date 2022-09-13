ddd1816627a928d735c7ae050595152a7e765bc1
Iam_fortunatus <amfortunatus@gmail.com>
2022-09-13 13:03:40 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include "main.h"
 
 /**
  * main - Prints _putchar.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
+	char *sh = "_putchar";
 
-	sz = sizeof(str) / sizeof(int);
-
-		_putchar(str[count]);
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
