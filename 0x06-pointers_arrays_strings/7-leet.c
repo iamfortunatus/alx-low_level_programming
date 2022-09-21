8e0be72c32a29e7258a91f278e8d2156513a4110
Iam_fortunatus <amfortunatus@gmail.com>
2022-09-21 19:23:14 +0100

    x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,24 +2,24 @@
 
 /**
  * leet - encode into 1337speak
- * @s: input value
+ * @str: input value
  * Return: s value
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
 	int count = 0, a;
 	int low_array[] = {97, 101, 111, 116, 108};
 	int upper_array[] = {65, 69, 79, 84, 76};
 	int num_array[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count) != '\0')
+	while (*(str + count) != '\0')
 	{
 		for (a = 0; a < 5; a++)
 		{
-			if (*(s + count) == low_array[i] || *(s + count) == upper_array[i])
+			if (*(str + count) == low_array[i] || *(str + count) == upper_array[i])
 			{
-				*(s + count) = num_array[a];
+				*(str + count) = num_array[a];
 				break;
 			}
 		}
