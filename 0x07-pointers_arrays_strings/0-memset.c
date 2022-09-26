d29c655b3eb54d72084999fe7733efec1bb45c3e
Iam_fortunatus <amfortunatus@gmail.com>
2022-09-26 21:11:28 +0100

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,11 @@
  * Return: the pointer to dest.
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *str, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
-	return (s);
+	for (i = 0; i < n ; ++i)
+		*(str + i) = b;
+	return (str);
 }
