11c74c97c9e06445a04db096a438d24b8a0df2d0
Iam_fortunatus <amfortunatus@gmail.com>
2022-10-04 23:14:00 +0100

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * *str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * Return: pointer to new space in memory or null
  **/
-
 char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
