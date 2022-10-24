b9f27508db69214b83c2ed22f5ce1e0c0b7ab8ce
Iam_fortunatus <amfortunatus@gmail.com>
2022-10-24 22:45:01 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,6 +8,23 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	h++;
-	return (0);
+	size_t c = 0;
+	listint_t *head, *temp;
+	long diff;
+
+	if (!h)
+		return (0);
+	head = *h;
+	*h = NULL;
+	while (head)
+	{
+		c++;
+		diff = head->next - head;
+		temp = head;
+		free(temp);
+		if (diff >= 0)
+			break;
+		head = head->next;
+	}
+	return (c);
 }
